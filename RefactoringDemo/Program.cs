42816f962a08e1af56e2b2199542536c17a7bf3a
Andrew Vozniuk <andrew.voznyuk@hys-enterprise.com>
2021-04-30 12:34:18 +0300

    changes

---
+++ RefactoringDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Refactoring.Lib;
+
+namespace RefactoringDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddUser(args);
+        }
+
+        public static void AddUser(string[] args)
+        {
+            var userService = new UserService();
+            var addResult = userService.AddUser("testName", "testLastName", "some@email.com",
+                DateTime.Now.AddYears(-25), 1);
+            Console.WriteLine($"Adding result: {addResult}");
+        }
+    }
+}
